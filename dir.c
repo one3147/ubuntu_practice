ebbe4bd84e7e2fe640ffb51fdaa208b222f4351f
one3147 <cantonetab0306@gmail.com>
2023-03-24 03:09:33 +0000

    Dir

---
+++ dir.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    DIR *dirp;
+    struct dirent *dentry;
+    
+    if((dirp == opendir(".")) == NULL) exit(1);
+
+    while (dentry = readdir(dirp)) {
+        if(dentry -> d_ino != 0)
+            printf("%s\n", dentry->d_name);
+    }
+    closedir(dirp);
+}
